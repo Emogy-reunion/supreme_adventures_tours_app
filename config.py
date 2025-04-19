b2f14faf1515b9c1765f24fa691dc9b1fc39e7c6
Emogy-reunion <mv7786986@gmail.com>
2025-04-19 23:02:27 +0300

    retrieve the database uri from the environment variables, the uri is stored in the uri to boost security

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 '''
 stores the application's configuration settings
 '''
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 class Config():
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
