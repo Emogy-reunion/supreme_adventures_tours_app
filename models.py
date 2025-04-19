839efde7d684e64e52052bf98381974c03636841
Emogy-reunion <mv7786986@gmail.com>
2025-04-19 07:31:51 +0300

    add: link the users and profiles tables to each other in a one to one relationship since the profiles table stores the user profile details

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 class Users(db.Model):
     '''
-    table that stores user details
+    table that stores user authentication details
     '''
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
@@ -19,9 +19,15 @@ class Users(db.Model):
     role = db.Column(db.String(50), default='member')
     verified = db.Column(db.Boolean, default=False)
     registered_on = db.Column(db.DateTime, default=datetime.utcnow)
+    profile = db.relationship('Profiles', uselist=False, backref='user', lazy='selectin')
 
 Class Profiles(db.Model):
+    '''
+    stores the user's profile information
+    '''
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+    bio = db.Column(db.Text, nullable=False)
     profile_picture = db.Column(db.String(200), nullable=True, default='default.jpg')
