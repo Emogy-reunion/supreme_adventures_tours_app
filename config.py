a8a312ba0d931d33ef5ad325cfd8e9215c3534eb
Emogy-reunion <mv7786986@gmail.com>
2025-04-19 23:14:29 +0300

    add a secret key that will be used to boost security since it is used during encryption and decrption, signing tokens, csrf tokens

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ import os
 
 load_dotenv()
 class Config():
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
