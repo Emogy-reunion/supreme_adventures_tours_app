d21c03b4e088ee15dfd380882f453a2d4fd0e51c
Emogy-reunion <mv7786986@gmail.com>
2025-04-20 06:53:54 +0300

    add: add configuration settings for the jwt tokens since they will be soleley used for user authentication

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ class Config():
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_TOKEN_LOCATION = ['cookies']
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    JWT_COOKIE_SECURE = True
+
