d8d3cfa36a6204558b51e27436bc6b35427a624a
Emogy-reunion <mv7786986@gmail.com>
2025-04-25 12:09:43 +0300

    create: add a form that validates the user input when creating an account"

---
+++ forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+
+class RegistrationForm(FlaskForm):
+    '''
+    validates data from the registration form
+    '''
+    first_name = StringField('First name', validators=[
+                DataRequired(),
+                Length(min=2, max=30, message='First name must be between 2 and 30 characters!')])
+    last_name = StringField('Last name', validators=[
+        DataRequired(),
+        Length(min=2, max=30, message='Last name must be between 2 and 30 characters!')])
+    email = StringField('Email', validators=[
+        InputRequired(),
+        Email(),
+        Length(min=4, max=45, message='Email must be between 4 and 45 characters!')])
+    username = StringField('Username', validators=[
+                DataRequired(message="Username is required"),
+                Length(min=3, max=25, message="Username must be between 3 and 25 characters!"),
+                Regexp('^\w+$', message="Username must contain only letters, numbers, or underscores!")])
+    phone_number = StringField('Phone number', validators=[
+        DataRequired(),
+        Regexp(r'^\+2547\d{8}$', message="Phone number must start with +2547 followed by 8 digits!")
+        ])
+    password = PasswordField('Password', validators=[
+        InputRequired(),
+        Length(min=8, message="Password must be at least 8 characters long!"),
+        Regexp(r'(?=.*[A-Z])', message="Password must contain at least one uppercase letter!"),
+        Regexp(r'(?=.*[a-z])', message="Password must contain at least one lowercase letter!"),
+        Regexp(r'(?=.*\W)', message="Password must contain at least one special character!")])
+    confirmpassword = PasswordField('Confirm password', validators=[
+        DataRequired(),
+        EqualTo('password', message='Passwords must match!')])
+
