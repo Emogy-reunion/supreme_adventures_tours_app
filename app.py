d572fc78ea78048e6a9fe5717575abb8cfa156f7
Emogy-reunion <mv7786986@gmail.com>
2025-04-26 11:56:30 +0300

    refactor: Ensured proper routing and application setup within the package structure

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from create_app import create_app
-from routes.authentication import auth
+from myapp import create_app
+from myapp.routes.authentication import auth
 
 
 app = create_app()
 app.register_blueprint(auth)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
