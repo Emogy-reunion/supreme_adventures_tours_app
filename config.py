8decfde3eac7886e9c308445c8ddfbcdabd8e1b5
Emogy-reunion <mv7786986@gmail.com>
2025-04-26 11:19:40 +0300

    Add configuration settings for celery to use redis as a message broker during running background and scheduled tasks

--- config.py
+++ config.py
@@ -14,4 +14,7 @@ class Config():
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_COOKIE_SECURE = True
+    CELERY_BROKER_URL = os.getenv("BROKER_URL")
+    CELERY_BACKEND_URL = os.getenv('BACKEND_URL')
+
 
