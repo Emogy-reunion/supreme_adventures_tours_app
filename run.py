c1652c29b2b2e199d5b8c00c43e45e3cecc3795f
Emogy-reunion <mv7786986@gmail.com>
2025-04-30 14:06:43 +0300

    add: register the reset blueprint with the app so that the route it defines can  access configuration settings

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from app import create_app
 from app.routes.authentication import auth
 from app.routes.email_verification import verify
+from app.routes.reset_password import reset
+
 
 
 app = create_app()
 app.register_blueprint(auth)
 app.register_blueprint(verify)
+app.register_blueprint(reset)
 
 if __name__ == "__main__":
     app.run(debug=True)
