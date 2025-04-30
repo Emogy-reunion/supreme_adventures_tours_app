21706630d6469e89fdaacea6fe98c705f04d03e3
Emogy-reunion <mv7786986@gmail.com>
2025-04-30 10:44:37 +0300

    add: create a route that verifies the token sent in the users verification email, if valid the users verification status is updated, if token is invalid, the user is redirected to a failure page

--- app/routes/verification.py
+++ app/routes/verification.py
@@ -1,5 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, current_app
+from app.models import Users, db
 
 
 verify = Blueprint('verify', __name__)
 
+@verify.route('/verify_email/<token>', methods=['POST'])
+def verify_email(token):
+    '''
+    verifies the email by verifying the token
+    '''
+    user = Users.verify_token(token)
+    if user:
+        try:
+            user.verified = True
+            db.session.commit()
+            return redirect(f'{current_app.config['FRONTEND_URL']}/success_page')
+        except Exception as e:
+            db.session.rollback()
+            return redirect(f'{current_app.config['FRONTEND_URL']}/failure_page')
+    else:
+        return redirect(f'{current_app.config['FRONTEND_URL']}/failure_page')
