f0f1c0904e341e7c6b2de8dbe9162ff9d416ba39
Emogy-reunion <mv7786986@gmail.com>
2025-04-30 13:51:04 +0300

    add: add a route that checks if a user exists and has been verified before sending a password reset email

--- app/routes/reset_password.py
+++ app/routes/reset_password.py
@@ -1,5 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.forms import EmailForm
+from app.background.password_reset_email import send_password_reset_email
 
 reset = Blueprint('reset', __name__)
 
+@reset.route('/reset_password', methods=['POST'])
+def reset_password():
+    '''
+    allows user to reset their password
+    '''
+    form = EmailForm(data=request.get_json())
+
+    if not form.validate():
+        return jsonify({"errors": form.errors}), 400
+
+    email = form.email.data
+
+    try:
+        user = Users.query.filter_by(email=email).first()
+
+        if user and user.verified:
+            send_password_reset_email.delay(user.id)
+        return jsonify({"success": "If the email is registered and verified, a password reset link will be sent."}), 200
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
+            
 
