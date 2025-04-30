31309608aa23fc2d575e4eddf8ef6d03da3a6e90
Emogy-reunion <mv7786986@gmail.com>
2025-04-30 14:10:44 +0300

    initialize the Flask jwt library so that it can access the configuration settings

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app
+from flask_jwt_extended import JWTManager()
 from app.routes.authentication import auth
 from app.routes.email_verification import verify
 from app.routes.reset_password import reset
@@ -6,6 +7,7 @@ from app.routes.reset_password import reset
 
 
 app = create_app()
+jwt = JWTManager(app)
 app.register_blueprint(auth)
 app.register_blueprint(verify)
 app.register_blueprint(reset)
