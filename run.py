b67f20e5a487a082a69385b7d1ac1f1a6b2550a6
Emogy-reunion <mv7786986@gmail.com>
2025-04-30 10:48:47 +0300

    register: register the verify blueprint so that it can access the app's configuration settings

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app
 from app.routes.authentication import auth
+from app.routes.email_verification import verify
 
 
 app = create_app()
 app.register_blueprint(auth)
+app.register_blueprint(verify)
 
 if __name__ == "__main__":
     app.run(debug=True)
