9d49261d7a9732876d363e3c886a6b95a89663bf
Emogy-reunion <mv7786986@gmail.com>
2025-05-01 13:14:24 +0300

    add: create a route verifies the password reset token and redirects user to a specific page depending on the verification results

--- app/routes/email_verification.py
+++ app/routes/email_verification.py
@@ -18,12 +18,12 @@ def verify_email(token):
         try:
             user.verified = True
             db.session.commit()
-            return redirect(f'{current_app.config['FRONTEND_URL']}/success_page')
+            return redirect(f"{current_app.config['FRONTEND_URL']}/success_page")
         except Exception as e:
             db.session.rollback()
-            return redirect(f'{current_app.config['FRONTEND_URL']}/failure_page')
+            return redirect(f"{current_app.config['FRONTEND_URL']}/failure_page")
     else:
-        return redirect(f'{current_app.config['FRONTEND_URL']}/failure_page')
+        return redirect(f"{current_app.config['FRONTEND_URL']}/failure_page")
 
 
 @verify.route('/resend_verification_email', methods=['POST'])
@@ -46,3 +46,20 @@ def resend_verification_email():
         return jsonify({"success": 'If an account with that email exists and is unverified, we are sending a new verification link — it should arrive shortly. Please check your email inbox!'}), 200
     except Exception as e:
         return jsonify({"error": 'An unexpected error occured. Please try again!'}), 500
+
+
+@verify.route('/verify_reset_password_token/<token>', methods=['POST'])
+def verify_reset_password_token(token):
+    '''
+    verifies the user by verifying the token sent via email
+    on success: the user is redirected to a page where they input their new passwords
+    on failure: the user is redirected to a failure page
+    '''
+    try:
+        user = Users.verify_token(token)
+        if user:
+            return redirect(f"{current_app.config['FRONTEND_URL']}/update_password")
+        else:
+            return redirect(f"{current_app.config['FRONTEND_URL']}/password_failure_page")
+    except Exception as e:
+        return redirect(f"{current_app.config['FRONTEND_URL']}/password_failure_page")
