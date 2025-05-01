df6182e4ee1fd5b3fe1604d1b71a92651fd22f0e
Emogy-reunion <mv7786986@gmail.com>
2025-05-01 12:47:52 +0300

    add: create a function that runs in the background to send a user a password reset link

---
+++ app/background/password_reset_email.py
@@ -0,0 +1,33 @@
+from flask import url_for, render_template, jsonify
+from app.backround.verification_email import mail
+from app.models import Users, db
+from flask_mail import Message
+from app.celery import make_celery
+from app import create_app
+
+app = create_app()
+celery = make_celery(app)
+
+@celery.task(bind=True, max_retries=3, default_retry_delay=10)
+def send_password_reset_email(self, user_id):
+    '''
+    sends an email that contains a link allowing user to reset password
+    '''
+    try:
+        user = db.session.get(Users, user_id)
+
+        if not user:
+            return {'error': 'User not found!'}
+
+        verification_token = user.email_verification_token()
+        verification_url = url_for('auth.verify_password_reset_token', verification_token=verification_token, _external=True)
+        msg = Message(
+                subject='Password reset link',
+                sender='info.realestate@gmail.com',
+                recipients=[user.email])
+        msg.body = f"We have recieved a password reset request, it it was you click the following link to reset your password {verification_url} otherwise ignore it"
+        msg.html = render_template('password_reset.html', user=user, verification_url=verification_url)
+        return ('success': 'Email sent successfully!'}
+    except Exception as e:
+        self.retry(exc=e, countdown=10, max_retries=3)
+         return {'error': 'An unexpected error occured. Please try again!'}
