b63d0b5876f6f502788694ef101694da97fc3ef2
Emogy-reunion <mv7786986@gmail.com>
2025-05-08 15:37:12 +0300

    add: create a route that will extract the token from the password reset link, verify it and redirect user according to what outcome they get

--- app/routes/email_verification.py
+++ app/routes/email_verification.py
@@ -45,4 +45,23 @@ def resend_verification_email():
 
         return jsonify({"success": 'If an account with that email exists and is unverified, we are sending a new verification link — it should arrive shortly. Please check your email inbox!'}), 200
     except Exception as e:
-        return jsonify({"error": 'An unexpected error occured. Please try again!'}), 500
+
+
+@verify.route('/verify_reset_password_token/<token>', methods=['POST'])
+def verify_reset_password_token(token):
+        '''
+        verifies the user by verifying the token sent via email
+        on success: the user is redirected to a page where they input their new passwords
+        on failure: the user is redirected to a failure page
+        '''
+        try:
+            user = Users.verify_token(token)
+            if user:
+                return redirect(f"{current_app.config['FRONTEND_URL']}/update_password/{user.id}")
+            else:
+                return redirect(f"{current_app.config['FRONTEND_URL']}/password_failure_page")
+            except Exception as e:
+                return redirect(f"{current_app.config['FRONTEND_URL']}/password_failure_page")
+
+
+
