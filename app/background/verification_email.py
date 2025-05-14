cbdb2cf3d2e392af4a403aeccd7b625db6318a34
Emogy-reunion <mv7786986@gmail.com>
2025-05-14 20:37:22 +0300

    fix: fix the typo error , correct the senders email

--- app/background/verification_email.py
+++ app/background/verification_email.py
@@ -16,7 +16,7 @@ def send_verification_email(self, user_id):
     '''
     try:
         user = db.session.get(Users, user_id)
-        if not users:
+        if not user:
             return {'error': 'User not found!'}
 
         verification_token = user.email_verification_token()
@@ -25,7 +25,7 @@ def send_verification_email(self, user_id):
         msg = Message(
                 subject='Verify your email',
                 recipients=[user.email],
-                sender='info.bytevision@gmail.com'
+                sender='infobytevision@gmail.com'
                 )
         msg.body = f'Click the following link to verify your email address {verification_url}'
         msg.html = render_template('verification_email.html', user=user, verification_url=verification_url)
