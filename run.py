83271d075712ac5e7fa0587f7093d1df3810b753
Emogy-reunion <mv7786986@gmail.com>
2025-05-14 09:58:32 +0300

    add: add logic that creates the tables in the database by calling the create_all function

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app
+from app.models import db, Users, Profiles
 from flask_jwt_extended import JWTManager()
 from app.routes.authentication import auth
 from app.routes.email_verification import verify
@@ -11,6 +12,9 @@ jwt = JWTManager(app)
 app.register_blueprint(auth)
 app.register_blueprint(verify)
 app.register_blueprint(reset)
+
+with app.app_context:
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
