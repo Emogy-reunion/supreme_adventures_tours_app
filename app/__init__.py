ea9fcc490ad0c776281a941c84881322b7928125
Emogy-reunion <mv7786986@gmail.com>
2025-05-14 11:21:35 +0300

    Refactor: Removed logic that passed app instance during SQLAlchemy and Bcrypt initialization, and moved initialization to run.py to avoid run time error

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from .config import Config
-from .models import db, bcrypt
-from .background.verification_email import mail
 
 def create_app():
     '''
@@ -10,7 +8,4 @@ def create_app():
     '''
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
-    bcrypt.init_app(app)
-    mail.init_app(app)
     return app
