95fbe56041bc35b0f74c836714299c2ce1ca5989
Emogy-reunion <mv7786986@gmail.com>
2025-05-15 11:40:03 +0300

    fix: correct the typo error and use the correct port for tls

--- app/config.py
+++ app/config.py
@@ -20,8 +20,8 @@ class Config():
     JWT_COOKIE_SECURE = False
     CELERY_BROKER_URL = os.getenv("BROKER_URL")
     CELERY_BACKEND_URL = os.getenv('BACKEND_URL')
-    MAIL_SERVER = 'smtp@gmail.com'
-    MAIL_PORT = 465
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_USE_TLS = True
