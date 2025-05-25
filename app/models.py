8af9392f9d32d401e895a01176c280797a1b8634
Emogy-reunion <mv7786986@gmail.com>
2025-05-25 20:20:04 +0300

    add: add other columns to the product table

--- app/models.py
+++ app/models.py
@@ -126,3 +126,14 @@ class Products(db.Model):
     stores merchandise sold via the app
     '''
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    product_type = db.Column(db.String(150), nullable=False)
+    original_price = db.Column(db.Float, nullable=False)
+    discount_rate = db.Column(db.Integer, default=0)
+    final_price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    size = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
