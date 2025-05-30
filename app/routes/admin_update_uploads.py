b4c0ad593c6304a9ba342a9050b151e364379679
Emogy-reunion <mv7786986@gmail.com>
2025-05-30 18:49:47 +0300

    refactor: optimize update logic to avoid unnecessary database writes  Added conditional checks before updating each field to ensure changes are only made if the incoming data differs from the existing values in the database Improves performance by minimizing redundant writes and reducing database overhead Enhances code clarity and prepares for more scalable update handling

--- app/routes/admin_update_uploads.py
+++ app/routes/admin_update_uploads.py
@@ -2,18 +2,18 @@ from flask import Blueprint, jsonify, request
 from app.utils.role import role_required
 from app.utils.discount import calculate_final_price
 from flask_jwt_extended import jwt_required
-from app.models import Tours, TourImages
+from app.models import Tours, TourImages, Products, ProductImages, db
 
 
 admin_edit_bp = Blueprint('admin_edit_bp', __name__)
 
-@admin_edit_bp.route('/update_tour/<int:tour_id>', methods=['PUT', 'PATCH'])
+@admin_edit_bp.route('/update_tour/<int:tour_id>', methods=['PATCH'])
 @jwt_required()
 @role_required('admin')
 def update_tour(tour_id):
     '''
     allows the admin to edit a specific tour
-    the admin can update the entire tour or just parts of it
+    it updates the entire resource so the put method is used
     '''
     form = UpdateTourForm(request.form)
 
@@ -40,52 +40,104 @@ def update_tour(tour_id):
         if not tour:
             return jsonify({'error': 'Tour not found'}), 404
 
-        if name:
+        if name and tour.name != name:
             tour.name = name
 
-        if start_location:
+        if start_location and tour.start_location != start_location:
             tour.start_location = start_location
 
-        if destination:
+        if destination and tour.destination != destination:
             tour.destination = destination
 
-        if start_date:
+        if start_date and tour.start_date != start_date:
             tour.start_date = start_date
 
-        if end_date:
+        if end_date and tour.end_date != end_date:
             tour.end_date = end_date
 
-        if days:
+        if days and tour.days != days:
             tour.days = days
 
-        if nights:
+        if nights and tour.nights != nights:
             tour.nights = nights
 
-        if original_price:
+        if original_price and tour.original_price != original_price:
             tour.original_price = original_price
             tour.final_price = original_price
 
-        if discount_percent:
+        if discount_percent and tour.discount_percent != discount_percent:
             tour.discount_percent = discount_percent
             tour.final_price = calculate_final_price(original_price=original_price, discount_percent=discount_percent)
 
-        if status:
+        if status and tour.status != status:
             tour.status = status
 
-        if included:
+        if included and tour.included != included:
             tour.included = included
 
-        if excluded:
+        if excluded and tour.excluded != excluded:
             tour.excluded = excluded
         db.session.commit()
         return jsonify({'success': 'Tour updated successfully!'}), 200
 
     except Exception as e:
         db.session.rollback()
-        return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
+        return jsonify({'error': 'An unexpected error occurred. Please try again!'}), 500
 
+@admin_edit_bp.route('/update_merchandise/<int:product_id>', methods=['PUT'])
+@jwt_required()
+@role_required('admin')
+def update_merchandise(product_id):
+    '''
+    updates a specific merchandise
+    updates the entire resource
+    '''
+    form = UpdateMerchandiseForm(request.form)
 
+    if not form.validate():
+        return jsonify({'error': form.errors}), 400
 
+    name = form.name.data
+    product_type = form.product_type.data
+    orginal_price = form.original_price.data
+    discount_rate = form.discount_rate.data
+    status = form.status.data
+    size = form.size.data
+    description = form.description.data
 
+    try:
+        product = Products.query.filter_by(id=product_id).first()
+
+        if not product:
+            return jsonify({'error': 'Product not found!'}), 404
+
+        if name:
+            product.name = name
+
+        if product_type:
+            product.product_type = product_type
+
+        if original_price:
+            product.original_price = original_price
+            final_price = original_price
+
+        if discount_percent:
+            product.discount_percent = discount_percent
+            product.final_price = calculate_final_price(original_price=original_price, discount_percent=discount_percent)
+        if status:
+            product.status = status
+
+        if size:
+            product.size = size
+
+        if description:
+            product.description = description
+
+        db.session.commit()
+        return jsonify({'success': 'Tour updated successfully!'}), 200
+
+     except Exception as e:
+         db.session.rollback()
+         return jsonify({'error': 'An unexpected error occurred. Please try again!'}), 500
 
 
