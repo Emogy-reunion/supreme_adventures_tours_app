3bc04f06cec97fa8d477d781180b8f8d33da924f
Emogy-reunion <mv7786986@gmail.com>
2025-06-02 11:32:06 +0300

    add: register the find blueprint so that the routes it defines can access the configuration settings

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ from app.routes.reset_password import reset
 from app.routes.admin_update_uploads import admin_edit_bp
 from app.routes.tours import tours_bp
 from app.routes.merchandise import merch_bp
+from app.routes.search import find
 import os
 from flask_cors import CORS
 from app.celery import make_celery
@@ -37,6 +38,7 @@ app.register_blueprint(post)
 app.register_blueprint(admin_edit_bp)
 app.register_blueprint(tours_bp)
 app.register_blueprint(merch_bp)
+app.register_blueprint(find)
 
 
 if __name__ == "__main__":
