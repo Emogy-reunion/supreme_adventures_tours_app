02b36e6abc1168c37ebf53dc0c59dc3e4dad5b99
Emogy-reunion <mv7786986@gmail.com>
2025-06-03 16:16:48 +0300

    create: create a route that allows admins to delete a specific product

--- app/routes/admin_delete.py
+++ app/routes/admin_delete.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from app.models import Tours, TourImages
+from app.models import Tours, TourImages, Products, ProductImages
 from app import db
 from flask_jwt_extended import jwt_required
 from app.utils.role import role_required
@@ -35,3 +35,26 @@ def delete_tour(tour_id):
         return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
 
 
+@admin_delete_bp.route('/delete_product/<int:product_id>', methods=['DELETE'])
+@jwt_required()
+@role_required('admin')
+def delete_product(product_id):
+    '''
+    deletes a specific product
+    '''
+    try:
+        product = Products.query.options(selectinload(Products.images)).filter_by(id=product_id).first()
+        if not product:
+            return jsonify({'error': 'Product not found!'}), 404
+
+        for image in tour.images:
+            upload_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+
+            if os.path.exists(upload_path):
+                os.remove(upload_path)
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'success': 'Product successfully deleted!'}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
