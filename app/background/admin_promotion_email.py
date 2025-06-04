755203d054347cc9605e9c172f57d41c2b98f70a
Emogy-reunion <mv7786986@gmail.com>
2025-06-04 12:40:36 +0300

    refactor: Added user firstname so  email can personalize content

--- app/background/admin_promotion_email.py
+++ app/background/admin_promotion_email.py
@@ -25,16 +25,16 @@ def send_admin_promotion_email(self, email):
                 recipients=[email]
                 )
         msg.body = (
-                "Hi,\n\n"
+                f"Hi {user.profile.first_name},\n\n"
                 "We’re excited to let you know that your role has been updated to 'Admin' on East Monarch.\n"
                 "You can now access admin features such as managing tours, merchandise, and other users.\n\n"
                 "If you were not expecting this change, please contact support immediately.\n\n"
                 "Thank you,\n"
                 "Supreme Adventures Team"
                                                                               )
-        msg.html = render_template('admin_promotion.html')
+        msg.html = render_template('admin_promotion.html', user)
         mail.send(msg)
         return {'success': 'Email sent successfully!'}
     except Exception as e:
-        self.retry(exc=e, max_retries=3, countdown=10)
+        self.retry(exc=e)
         return {'error': 'Email not sent because of an unexpected error!'}
