0b5139980417ab54d6fc9cccec61965e0f316fe0
Emogy-reunion <mv7786986@gmail.com>
2025-06-10 11:34:33 +0300

    create: add a route that lets logged in users to send email messages to the admin

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request, render_template
-from app import mail, create_app
+from app import mail, create_app, db
 from flask_mail import Message
-from app.forms import GuestContactForm
+from app.forms import GuestContactForm, MemberContactForm
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models import Users
 
 
 app = create_app()
@@ -41,3 +43,37 @@ def guest_contact():
     except Exception as e:
         return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
 
+
+
+@contact_bp.route('/member_contact', methods=['POST'])
+@jwt_required()
+def member_contact():
+    form = MemberContactForm(data=request.get_json())
+
+    if not form.validate():
+        return jsonify({'errors': form.errors}), 400
+
+    message = form.message.data
+    try:
+        user_id = get_jwt_identity()
+        user = db.session.get(Users, user_id)
+
+        if not user:
+            return jsonify({'error': 'User not found!'}), 404
+
+        msg = Message(
+                subject='Tour app inquiry',
+                sender=app.config['DEFAULT_MAIL_SENDER'],
+                recipients=[user.email]
+                )
+        msg.body =  (
+                "New inquiry from Tour App:\n\n"
+                f"Name: {user.first_name}\n"
+                f"Email: {user.email}\n\n"
+                f"Message:\n{message}\n"
+                )
+        msg.html = render_template('member_contact.html', user=user)
+        mail.send(msg)
+        return jsonify({'success': "Thank you for your message. Our team will get back to you as soon as possible."}), 200
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
