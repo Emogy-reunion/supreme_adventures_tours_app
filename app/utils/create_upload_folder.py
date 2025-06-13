5997e94f6a00bd6f5f6c1cf69ec9258ad7f7beee
Emogy-reunion <mv7786986@gmail.com>
2025-06-13 13:45:53 +0300

    fix: correctly create uploads folder by passing app context to utility function

--- app/utils/create_upload_folder.py
+++ app/utils/create_upload_folder.py
@@ -3,13 +3,12 @@ creates the upload folder if it doesn't exist
 '''
 import os
 
-def create_upload_folder():
+def create_upload_folder(app):
     '''
     checks it the uploads folder is available
     if not it is created
     if it exists nothing happens
     '''
-    def create_upload_folder():
-            upload_path = os.path.join(app.root_path, 'static', 'uploads')
-            if not os.path.exists(upload_path):
-                os.makedirs(upload_path)
+    upload_path = os.path.join(app.root_path, 'static', 'uploads')
+    if not os.path.exists(upload_path):
+        os.makedirs(upload_path)
