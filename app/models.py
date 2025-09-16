514a63934bcd14388318c8c27dfddf6e9bdeda73
Emogy-reunion <mv7786986@gmail.com>
2025-09-16 08:44:03 +0300

    refactor: remove the relationship between the tours table and tour images table since a tour won't be having images just the poster

--- app/models.py
+++ app/models.py
@@ -114,7 +114,6 @@ class Tours(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     user = db.relationship('Users', back_populates='tours')
-    images = db.relationship('TourImages', backref='tour', cascade='all, delete', lazy='selectin')
     poster = db.relationship('Posters', backref='tour', cascade='all, delete', lazy='selectin', uselist=False)
     bookings = db.relationship('Bookings', back_populates='tour', lazy='selectin')
 
