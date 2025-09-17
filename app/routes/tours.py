4f433191f8b1f972e4ae9169b79b26c72cd5efbb
Emogy-reunion <mv7786986@gmail.com>
2025-09-17 11:00:36 +0300

    fix: fix the syntax error by adding the missing comma

--- app/routes/tours.py
+++ app/routes/tours.py
@@ -35,7 +35,7 @@ def tours():
             'status': tour.status.title(),
             'included': tour.included,
             'excluded': tour.excluded,
-            'poster': tour.poster.poster if tour.poster else None
+            'poster': tour.poster.poster if tour.poster else None,
             'image': tour.preview.filename if tour.preview else None
             } for tour in paginated_results.items]
 
