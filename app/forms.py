707deef1082ad695051f7f62764cabc60337f7ed
Emogy-reunion <mr_robot@emogyreunion.localdomain>
2025-11-16 16:00:25 +0300

    add: add a class that will be used to perform validation checks on the destination data

--- app/forms.py
+++ app/forms.py
@@ -131,6 +131,30 @@ class ToursUploadForm(FlaskForm):
         ])
 
 
+class DestinationUploadForm(FlaskForm):
+    '''
+    validates the destination upload details for the form
+    '''
+    name = StringField('Name', validators=[
+        DataRequired(),
+        Length(min=4, max=60, message='Destination name must be betwwen 4 and 60 characters!')])
+    country = StringField('Country', validators=[
+        DataRequired(),
+        Length(min=2, max=60, message='Country name must be betwwen 2 and 60 characters!')])
+    destination_type = StringField('Destination type', validators=[
+        DataRequired(),
+        Length(min=2, max=60, message='Destination type must be betwwen 2 and 60 characters!')])
+    short_description = TextAreaField('Short Description', validators=[
+        DataRequired(),
+        length_check(100, 150, "Short description")])
+    long_descrtiption = TextAreaField('Long Description', validators=[
+        DataRequired(),
+        length_check(750, 1500, "Long description")])
+    main_activities = TextAreaField('Main activities', validators=[
+        DataRequired(),
+        length_check(30, 800, "Main activities")])
+
+
 class ProductsUploadForm(FlaskForm):
     '''
     validates the upload details for the form
